49b79f02a220c5237b28a7d1f77a044412265b83
Mạnh Nguyễn <manhnd.jin@gmail.com>
2023-06-25 16:59:31 +0700

    chore: add swagger

--- apps/account-service/src/main.ts
+++ apps/account-service/src/main.ts
@@ -9,12 +9,23 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app/app.module';
 import {MicroserviceOptions, Transport} from "@nestjs/microservices";
 import {RabbitmqConfig, rabbitmqConfig} from "@all-in-one/account/utils/config";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose']
   });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Account service')
+    .setDescription('The account service API description')
+    .setVersion('1.0')
+    .addTag('account')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   const accountConfigService = app.get<RabbitmqConfig>(rabbitmqConfig.KEY);
 
@@ -28,10 +39,12 @@ async function bootstrap() {
       },
     }
   });
-
   await app.startAllMicroservices();
   Logger.log('Account service is running');
 
+  await app.listen(3300, () => {
+    Logger.log('Account service is listening on port 3300');
+  });
 }
 
 bootstrap();
