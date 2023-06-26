97ef6699ef75c0229c1d9abe22eaae597d6fdf38
Mạnh Nguyễn <manhnd.jin@gmail.com>
2023-06-27 00:25:58 +0700

    feat(account): add database module and account command module to the account service

--- apps/account-service/src/app/app.module.ts
+++ apps/account-service/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {AccountConfigModule} from "./account-config.module";
 import {AccountDatabaseModule} from "@all-in-one/account/database";
+import {AccountCommandsModule} from "@all-in-one/account/commands";
 
 @Module({
-  imports: [AccountConfigModule, AccountDatabaseModule],
+  imports: [AccountConfigModule, AccountDatabaseModule, AccountCommandsModule],
   controllers: [AppController],
   providers: [AppService],
 })
