b5ce350f90c511a63200049ff6aa7fa63b8b9ace
Mạnh Nguyễn <manhnd.jin@gmail.com>
2023-06-27 00:27:12 +0700

    chore: add application core

---
+++ libs/core/application/src/lib/context/AppRequestContext.ts
@@ -0,0 +1,22 @@
+import {RequestContext} from "nestjs-request-context";
+
+export class AppRequestContext extends RequestContext {
+  // @ts-ignore
+  requestId: string;
+}
+
+export class RequestContextService {
+  static getContext(): AppRequestContext {
+    const ctx: AppRequestContext = RequestContext.currentContext.req;
+    return ctx;
+  }
+
+  static setRequestId(id: string): void {
+    const ctx = this.getContext();
+    ctx.requestId = id;
+  }
+
+  static getRequestId(): string {
+    return this.getContext().requestId;
+  }
+}
