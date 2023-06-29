1c88865e8915716ee693af203ab3a1640f61bdda
Mạnh Nguyễn <manhnd.jin@gmail.com>
2023-06-29 17:12:10 +0700

    chore: add validation logic for request id

--- libs/core/application/src/lib/context/AppRequestContext.ts
+++ libs/core/application/src/lib/context/AppRequestContext.ts
@@ -16,6 +16,15 @@ export class RequestContextService {
   }
 
   static getRequestId(): string {
-    return this.getContext().requestId;
+    const requestId = this.getContext().requestId;
+    if (!this.requestIdNotEmptyGuard(requestId)) {
+      throw new Error('Request id is not set');
+    }
+
+    return requestId;
+  }
+
+  private static requestIdNotEmptyGuard(id: string | null): id is string {
+    return id !== null;
   }
 }
